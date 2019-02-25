ddbd448aaa6652ee24eaa98ae468acd14e32043c
ZackaryWelch <welch.zackary@gmail.com>
2019-02-25 13:04:05 -0500

    Improved Google Sheets integration and added args for token and spreadsheet_id

--- stats.py
+++ stats.py
@@ -14,10 +14,9 @@ from github import Github
 def get_referrers(repo):
     referrers = repo.get_top_referrers()
     values = []
+    values.append(['referrers'])
     for referrer in referrers:
-        value = [referrer.count, referrer.uniques, referrer.referrer]
-        values.append(value)
-    print(values)
+        values.append(['', referrer.count, referrer.uniques, '', referrer.referrer])
     return values
 
 def print_referrers(repo, f):
@@ -30,10 +29,9 @@ def print_referrers(repo, f):
 def get_paths(repo):
     paths = repo.get_top_paths()
     values = []
+    values.append(['paths'])
     for path in paths:
-        value = [path.count, path.uniques, path.path]
-        values.append(value)
-    print(values)
+        values.append(['', path.count, path.uniques, '', path.path])
     return values
 
 def print_paths(repo, f):
@@ -49,16 +47,16 @@ def get_views(repo):
     total_views = daily_views['count']
     total_unique_views = daily_views['uniques']
     values = []
-    values.append([total_views, total_unique_views])
+    values.append(['views: total_count', total_views, total_unique_views])
     for view in daily_views['views']:
         formatted_time = view.timestamp.strftime("%m/%d/%y")
-        values.append([view.count, formatted_time])
+        values.append(['', view.count, '', formatted_time])
     for view in weekly_views['views']:
         week_start = view.timestamp
         week_end = week_start + datetime.timedelta(days=7)
         formatted_start = week_start.strftime("%m/%d/%y")
         formatted_end = week_end.strftime("%m/%d/%y")
-        values.append([view.count, formatted_start + '-' + formatted_end])
+        values.append(['', view.count, '', formatted_start + '-' + formatted_end])
     return values
 
 def print_views(repo, f):
@@ -83,10 +81,11 @@ def get_clones(repo):
     clones = repo.get_clones_traffic()
     total_count = clones['count']
     values = []
-    values.append([total_count])
+    #Adds a row with the total count of all clones
+    values.append(['clones: total_count', total_count])
     for clone in clones['clones']:
         formatted_time = clone.timestamp.strftime("%m/%d/%y")
-        values.append[clone.count, clone.uniques, formatted_time]
+        values.append(['', clone.count, clone.uniques, formatted_time])
     return values
 
 def print_clones(repo, f):
@@ -94,46 +93,85 @@ def print_clones(repo, f):
     total_count = clones['count']
     f.write(f'Total clones: {total_count}\n')
     for clone in clones['clones']:
+        #Converts the datetime format to m/d/y to remove pointless h/m/s value
         formatted_time = clone.timestamp.strftime("%m/%d/%y")
         f.write(f'Total clones: {clone.count}, Unique clones: {clone.uniques}, on {formatted_time}\n')
     f.write('\n')
 
 def main():
-    parser = argparse.ArgumentParser(description='Retrieve traffice data from iocage plugin repos on Github')
-    parser.add_argument('-file', default=True, action='store_true', help='Write to file')
-    parser.add_argument('-sheets', default=False, action='store_true', help='Write to Google Sheets (requires credentials.json)')
-    parser.add_argument('-i', default=False, action='store_true', help='Create individual files')
-    parser.add_argument('-s', default=True, action='store_true', help='Create a single file (default)')
-    parser.add_argument('-r', default=True, action='store_true', help='Only get referrers')
-    parser.add_argument('-v', default=True, action='store_true', help='Only get views')
-    parser.add_argument('-p', default=True, action='store_true', help='Only get paths')
-    parser.add_argument('-c', default=True, action='store_true', help='Only get clones')
-    parser.add_argument('-verbose', default=False, action='store_true', help='Verbose mode')
-    args = parser.parse_args()
-    write_file = args.file
-    write_sheets = args.sheets
-    individual = args.i
-    single = args.s
-    only_referrers = args.r
-    only_views = args.v
-    only_paths = args.p
-    only_clones = args.c
-    verbose = args.verbose
+    print_dict = {'referrers': print_referrers,
+                  'paths': print_paths,
+                  'views': print_views,
+                  'clones': print_clones}
+    get_dict = {'referrers': get_referrers,
+                'paths': get_paths,
+                'views': get_views,
+                'clones': get_clones}
 
-    #Put access token in here
-    g = Github("")
+    parser = argparse.ArgumentParser(description='Retrieve traffic data from iocage plugin repos on Github')
+    parser.add_argument('-format', default='file', help='Output format (file or sheets)')
+    parser.add_argument('-file_type', default='single', help='Choose type of file output (single or individual)')
+    parser.add_argument('-sheet_id', default='', help='The ID of the spreadsheet to be written to. By default a new spreadsheet is created. Must have write access to the sheet.')
+    parser.add_argument('-grabs', nargs="+", default=list(print_dict), help=f'Choose which data points to retrieve, default all: {" ".join(list(print_dict))}')
+    parser.add_argument('-v', default=False, action='store_true', help='Verbose mode')
+    parser.add_argument('-token', default='', help='Access token for GitHub. Must have push access to repo')
+    args = parser.parse_args()
+    format_type = args.format
+    file_type = args.file_type
+    grabs = args.grabs
+    verbose = args.v
+    inputted_id = args.sheet_id
+    access_token = args.token
+
+    if access_token == '':
+        sys.exit("No token inputted. Cannot connect to GitHub.")
+
+    #Explicitly grab all iocage-plugin repos from the freenas org
+    #Possibly could provide organization name or repo name as args
+    g = Github(access_token)
     org = g.get_organization("freenas")
     plugin_repos = [repo for repo in org.get_repos() if 'iocage-plugin' in repo.name]
 
     if verbose:
         print("Total plugins:", len(plugin_repos))
 
-    if write_sheets:
+    #Accept shorter arguments and check for invalid input
+    grab_names = [name for name in grabs]
+    total_grabs = []
+    for grab in grab_names:
+        if grab == 'r' or grab == 'referrers':
+            total_grabs.append('referrers')
+        elif grab == 'v' or grab == 'views':
+            total_grabs.append('views')
+        elif grab == 'p' or grab == 'paths':
+            total_grabs.append('paths')
+        elif grab == 'c' or grab == 'clones':
+            total_grabs.append('clones')
+        else:
+            print(f'Unknown grab function {grab}, skipping')
+    if file_type == 'i':
+        file_type = 'individual'
+    if file_type == 's':
+        file_type = 'single'
+    if file_type != 'single' and file_type != 'individual':
+        print(f'Invalid file type {file_type}')
+    if format_type == 's':
+        format_type = 'sheets'
+    if format_type == 'f':
+        format_type = 'file'
+    if format_type != 'sheets' and format_type != 'file':
+        print(f'Invalid output format {format_type}')
+
+    if format_type=='sheets':
+        #R/W scope. Full scope list on Google API docs
         scopes = ['https://www.googleapis.com/auth/spreadsheets']
-        #Put the ID of the spreasheet to write to here
         sheet_id = ''
+        #Sheet and range for the spreadsheet. This script uses all of Sheet1
         sheet_range = 'Sheet1'
+        sheet = None
         creds = None
+        #Loads credentials from the token if the script has ran before
+        #Otherwise they are loaded from credentials.json, then a token is created
         if os.path.exists('token.pickle'):
             with open('token.pickle', 'rb') as token:
                 creds = pickle.load(token)
@@ -148,43 +186,53 @@ def main():
                 pickle.dump(creds, token)
 
         service = build('sheets', 'v4', credentials=creds)
-        sheet = service.spreadsheets()
+        if inputted_id == '':
+            #Creates a spreadsheet with the title GitHub Data if no id is given
+            body = {'properties': { 'title': 'GitHub Data' } }
+            sheet = service.spreadsheets().create(body=body, fields='spreadsheetId').execute()
+            sheet_id = sheet.get('spreadsheetId')
+        else:
+            sheet_id = inputted_id
+        #Create a header row on the spreadsheet
+        values = [['repo', 'count', 'uniques', 'date', 'name']]
         for repo in plugin_repos:
+            #Add the name as a row for each plugin repo
+            values.append([repo.name])
             if verbose:
                 print("Traffic data for", repo.name, end=' ')
-            values = get_referrers(repo)
-            values.append(get_paths(repo))
-            values.append(get_views(repo))
-            values.append(get_clones(repo))
-            if verbose:
-                print(' ')
-        body = { 'values' : values } 
-        result = sheet.values().update(spreadsheetId=sheet_id, range=sheet_range, valueInputOption='USER_ENTERED', body=body).execute()
-    elif write_file:
-        f = None
-        if single:
-            f = open('data_plugins', 'w')
-        for repo in plugin_repos:
-            if individual:
-                f = open('data_'+repo.name, 'w')
-            elif single:
-                f.write(repo.name+'\n\n')
-            if verbose:
-                print("Traffic data for", repo.name, end=' ')
-            if only_referrers:
-                print_referrers(repo, f)
-            if only_paths:
-                print_paths(repo, f)
-            if only_views:
-                print_views(repo, f)
-            if only_clones:
-                print_clones(repo, f)
+            for name in total_grabs:
+                #Generate rows of data from api calls in the get functions
+                value_list = get_dict[name](repo)
+                for value in value_list:
+                    values.append(value)
             if verbose:
                 print(' ')
                 sys.stdout.flush()
-            if individual:
+        sheet = service.spreadsheets()
+        body = { 'values' : values }
+        #Send the cell values to the sheet
+        result = sheet.values().update(spreadsheetId=sheet_id, range=sheet_range, valueInputOption='USER_ENTERED', body=body).execute()
+    else:
+        #Writes files with the name data_REPO if individual format
+        #Otherwise data_plugins for a single file
+        f = None
+        if file_type == 'single':
+            f = open('data_plugins', 'w')
+        for repo in plugin_repos:
+            if file_type == 'individual':
+                f = open('data_'+repo.name, 'w')
+            elif file_type == 'single':
+                f.write(repo.name+'\n\n')
+            if verbose:
+                print("Traffic data for", repo.name, end=' ')
+            for name in total_grabs:
+                print_dict[name](repo, f)
+            if verbose:
+                print(' ')
+                sys.stdout.flush()
+            if file_type == 'individual':
                 f.close()
-        if single:
+        if file_type == 'single':
             f.close()
 
 if __name__ == "__main__": main()
